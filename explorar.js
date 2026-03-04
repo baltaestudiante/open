22e3aed98ea8b496905c9f9072d63216e7128034
baltaestudiante <balta.estudiante@gmail.com>
2026-03-04 17:39:16 -0600

    Update explorar.js

--- explorar.js
+++ explorar.js
@@ -1,8 +1,3 @@
 window.renderExplorar = function() {
-    return `
-        <div class="text-center py-10">
-            <h1 class="text-3xl font-bold mb-6">Explorar</h1>
-            <p class="text-gray-400">Aquí irá el contenido de explorar (categorías, destacados, etc.)</p>
-        </div>
-    `;
+    return '<div>Contenido de explorar</div>';
 };
