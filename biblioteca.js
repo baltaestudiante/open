4609025b1dd7ebabc40a0ece0510c845c49c6ff3
baltaestudiante <balta.estudiante@gmail.com>
2026-03-04 17:05:35 -0600

    Create biblioteca.js

---
+++ biblioteca.js
@@ -0,0 +1,8 @@
+window.renderBiblioteca = function() {
+    return `
+        <div class="text-center py-10">
+            <h1 class="text-3xl font-bold mb-6">Biblioteca</h1>
+            <p class="text-gray-400">Lista de episodios guardados, etc.</p>
+        </div>
+    `;
+};
